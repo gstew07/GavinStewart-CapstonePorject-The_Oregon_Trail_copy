3f572cb9a074065dce04bffcb18930aaf1e0a86e
gstew07 <gstew07@outlook.com>
2024-12-02 15:21:02 -0600

    finished the far scrolling background
    I finished the first scrolling background and started on a scrolling base for them to walk on

--- The Oregon trail copy/sketch.js
+++ The Oregon trail copy/sketch.js
@@ -9,48 +9,49 @@ let mainBackground, font;
 let titleFade = 1, startFade = 0, startFadeTime = 0;
 let screenCode = 0, backgroundPos = 0, totalChange, position = 0;
 
-function preload(){
+function preload() {
   mainBackground = loadImage("assets/mainBackground.jpg");
   mountains = loadImage("assets/mountains.jpg");
   inverseMountains = loadImage("assets/mountains2.jpg");
+  grassGround = loadImage("assets/grassGround.png");
   font = loadFont("assets/pixelFont.ttf");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont(font);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(220);
-  if(screenCode === 0) titleScreen();
-  if(screenCode === 1) startVillage();
+  if (screenCode === 0) titleScreen();
+  if (screenCode === 1) startVillage();
 
 }
 
-function titleScreen(){
+function titleScreen() {
   background(mainBackground);
   titleFade += 3.5;
-  
+
   textSize(70);
   textStyle(BOLD);
   fill(0, 0, 0, titleFade);
-  text("The Oregon Trail", width/2, height/8 + 7);
+  text("The Oregon Trail", width / 2, height / 8 + 7);
 
   fill(24, 192, 47, titleFade);
-  text("The Oregon Trail", width/2, height/8);
-  if(titleFade > 300){
-    if(startFadeTime === 0 && startFade < 1){
+  text("The Oregon Trail", width / 2, height / 8);
+  if (titleFade > 300) {
+    if (startFadeTime === 0 && startFade < 1) {
       startFade += 0.02;
     }
-    else if(startFadeTime === 0){
+    else if (startFadeTime === 0) {
       startFadeTime = 1;
     }
-    if(startFadeTime === 1 && startFade > 0){
+    if (startFadeTime === 1 && startFade > 0) {
       startFade -= 0.02;
     }
-    else if(startFadeTime === 1){
+    else if (startFadeTime === 1) {
       startFadeTime = 0;
     }
 
@@ -58,39 +59,46 @@ function titleScreen(){
 
     textSize(60);
     fill(255, 255, 255, fade);
-    text("PRESS SPACE TO START", width/2, 3*height/4);
-    if(keyIsPressed && key === " "){
+    text("PRESS SPACE TO START", width / 2, 3 * height / 4);
+    if (keyIsPressed && key === " ") {
       screenCode = 1;
-      backgroundPos = -10*width;
-      totalChange = 0;
+      backgroundPos = -(5 * 2) * width;
       position += backgroundPos;
+      totalChange = 0;
+
     }
-    
+
   }
 }
 
-function startVillage(){
-  rotateBackground1(mountains, inverseMountains, 5);
-  
+function startVillage() {
+  rotateBackground1(mountains, inverseMountains, grassGround, 5);
+
 }
 
-function rotateBackground1(image1, image2, rate){
-  rotateBackground2(image1, image2, position, 1);
-  if(position <= 0 - rate){
+function rotateBackground1(image1, image2, image3, rate) { //sets up the scrolling background
+
+  rotateBackground2(image1, image2, image3,  position, 1);
+  if (position <= 0 - rate) {
     position += rate;
     totalChange += rate;
   }
 }
 
-function rotateBackground2(image1, image2, xPosition, direction){
-  if(xPosition <= totalChange){
-    if(direction === 1){
+function rotateBackground2(image1, image2, image3, xPosition, direction) {// uses two images and reccurssion to scroll the background
+  if (xPosition <= totalChange) {
+    if (direction === 1) {
       image(image1, xPosition, 0, width, height);
+
     }
-    else if(direction === -1){
+    else if (direction === -1) {
       image(image2, xPosition, 0, width, height);
+
     }
-    
-    rotateBackground2(image1, xPosition + width, direction*-1);
+    image(image3, xPosition, 9*height/10, width, 9*height/10);
+    rotateBackground2(image1, image2, image3, xPosition + width, direction * -1);
+
+
+
   }
 }
